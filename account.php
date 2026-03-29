b900604c5cb313e3533baa985fb58e6a099d56a1
Ephram <ephrambeauhall@gmail.com>
2026-03-29 11:25:00 +0100

    copyright

--- account.php
+++ account.php
@@ -2,3 +2,7 @@
 <?php session_start(); ?>
 <h1>Account</h1>
 <p>Welcome <?=$_SESSION['user']?></p>
+<footer style="position:fixed; left:0; right:0; bottom:0; display:flex; justify-content:space-between; padding:8px 12px; font-size:11px; color:rgba(0,0,0,0.65);">
+	<span>Designed by @Ephraxm</span>
+	<span>@soulaero 2026</span>
+</footer>
--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-let counter = 0; let Title = 'Ephraxm | IG'; let direction = true;
+let counter = 0; let Title = 'oulaero'; let direction = true;
 aniTitle = setInterval(function () {
     if (counter == Title.length) direction = false;
     if (counter == false) direction = true;
     counter = (direction == true) ? ++counter : --counter;
     newtitle = (counter == 0) ? "" : Title.slice(0, counter);
-    document.title = '@' + newtitle;
-}, 400)
+    document.title = 's' + newtitle;
+}, 700)
 
 function disableImageDragging() {
     const images = document.querySelectorAll('img');
